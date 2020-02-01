53dbcc381eb7095d2afd2ee4ad1661d624a0ca56
DCorbellini <DCorbellini@users.noreply.github.com>
2020-01-31 22:33:01 -0300

    Agrega clases de Bootstrap

--- src/main.js
+++ src/main.js
@@ -33,12 +33,6 @@ $boton.onclick = () => {
 }
 
 
-function crearOpcionBase(key) {
-    const nuevaOpcion = document.createElement('option')
-    nuevaOpcion.value = key
-    nuevaOpcion.innerText = key
-    $seleccionarBase.appendChild(nuevaOpcion)
-}
 function manejarOpciones(respuestaJSON) {
     Object.keys(respuestaJSON.rates).forEach(moneda => {
         crearOpcionBase(moneda)
@@ -46,12 +40,38 @@ function manejarOpciones(respuestaJSON) {
     $seleccionarBase.value = respuestaJSON.base
     $seleccionarFecha.value = respuestaJSON.date
 }
+function crearOpcionBase(key) {
+    const nuevaOpcion = document.createElement('option')
+    nuevaOpcion.value = key
+    nuevaOpcion.innerText = key
+    $seleccionarBase.appendChild(nuevaOpcion)
+}
 function manejarLista(respuestaJSON) {
     $lista.innerHTML = ''
     Object.keys(respuestaJSON.rates).forEach(moneda => {
-        const rate = document.createElement('li')
-        rate.innerText = `${moneda}: ${respuestaJSON.rates[moneda]}`
-        $lista.appendChild(rate)
+        const nuevoItem = document.createElement('div')
+        nuevoItem.classList.add('row')
+        nuevoItem.classList.add('item')
+        const nuevaMoneda = document.createElement('div')
+        nuevaMoneda.classList.add('col-sm-1')
+        nuevaMoneda.classList.add('list-group-item')
+        nuevaMoneda.classList.add('item')
+        nuevaMoneda.innerText = moneda
+        const nuevoValor = document.createElement('div')
+        nuevoValor.classList.add('col-sm-4')
+        nuevoValor.classList.add('list-group-item')
+        nuevoValor.classList.add('item')
+        nuevoValor.innerText = respuestaJSON.rates[moneda]
+
+        nuevoItem.appendChild(nuevaMoneda)
+        nuevoItem.appendChild(nuevoValor)
+        $lista.appendChild(nuevoItem)
+
+        
+        //`${moneda}: ${respuestaJSON.rates[moneda]}`
+
+
+        //$lista.appendChild(rate)
     })
 }
 
